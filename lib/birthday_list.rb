0ef8e91a86ee01371a7bf1b2ef5b69e4ca64a79e
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-02 12:40:56 +0100

    Added feature to add a person and a birthday date to the list

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,12 @@
 
+class Birthday_list
+  attr_reader :birthday_list
+
+  def initialize
+    @birthday_list = []
+  end
+
+  def add_birthday(name, birthday)
+    @birthday_list << {name: name, birthday: birthday}
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,11 @@
+require 'birthday_list'
 
+describe Birthday_list do 
+  describe '#add_birthday' do
+    it 'adds a birthday with name and date to the list' do
+      birthday_list = Birthday_list.new
+      birthday_list.add_birthday('Nata','18/03/1989') 
+      expect(birthday_list.birthday_list).to eq([{name: 'Nata', birthday: '18/03/1989'}])
+    end
+  end
+end
